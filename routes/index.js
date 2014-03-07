9d76192b61fed437f47b04f98e6a84a6e9cdc781
RicterZ <RicterZheng@gmail.com>
2014-03-07 21:10:10 +0800

    for angualr

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var Auth = require('../controller/authorization'),
     ArticleHandler = require('../controller/articles'),
-    PageHandler = require('../controller/page'),
-    fs = require('fs');
+    PageHandler = require('../controller/page');
 
 
 // Auth.auth is a middleware of Token Authorization.
 
 module.exports = function(app) {
     // router of index
-    app.get('/', function(req, res){res.end(fs.readFileSync("./public/index.html", "binary"))});
+    app.get('/', function(req, res){res.redirect('/index.html')});
     app.get('/pages/:id', PageHandler.show_page);
 
     // router of /article
